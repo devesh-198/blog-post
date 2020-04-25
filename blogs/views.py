a8a2bbdbad4f9993ec70508ff8ad5f7c8caa593b
Devesh Gupta <devesh.gupta.198@gmail.com>
2020-04-25 13:36:26 +0530

    Added custom 404 and 500 error page.

--- blogs/views.py
+++ blogs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.http import Http404
 
@@ -34,7 +34,7 @@ def new_blog(request) :
 @login_required
 def edit_blog(request, blog_id) :
     """page to edit a blog"""
-    blog = BlogPost.objects.get(id=blog_id)
+    blog = get_object_or_404(BlogPost, id=blog_id)
     if blog.owner != request.user :
         raise Http404
 
